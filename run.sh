1a7677543e72a68f87236ab6c07e648207314d15
Gumy Nicolas <Nicolas.Gumy@swisscom.com>
2023-01-20 13:32:13 +0100

    fix permission

--- run.sh
+++ run.sh
@@ -13,11 +13,11 @@ sudo chmod -R 777 $DESTINATION/etc
 sudo chmod -R 777 $DESTINATION/postgresql
 
 # config
-if grep -qF "fs.inotify.max_user_watches" /etc/sysctl.conf; then echo $(grep -F "fs.inotify.max_user_watches" /etc/sysctl.conf); else echo "fs.inotify.max_user_watches = 524288" | sudo tee -a /etc/sysctl.conf; fi
-sudo sysctl -p
-sed -i 's/10016/'$PORT'/g' $DESTINATION/docker-compose.yml
-sed -i 's/20016/'$CHAT'/g' $DESTINATION/docker-compose.yml
+# if grep -qF "fs.inotify.max_user_watches" /etc/sysctl.conf; then echo $(grep -F "fs.inotify.max_user_watches" /etc/sysctl.conf); else echo "fs.inotify.max_user_watches = 524288" | sudo tee -a /etc/sysctl.conf; fi
+# sudo sysctl -p
+sed -i 's/10016/'$PORT'/g' "$DESTINATION/docker-compose.yml"
+sed -i 's/20016/'$CHAT'/g' "$DESTINATION/docker-compose.yml"
 # run Odoo
-docker-compose -f $DESTINATION/docker-compose.yml up -d
+docker-compose -f "$DESTINATION/docker-compose.yml" up -d
 
 echo 'Started Odoo @ http://localhost:'$PORT' | Master Password: gumy.love | Live chat port: '$CHAT
